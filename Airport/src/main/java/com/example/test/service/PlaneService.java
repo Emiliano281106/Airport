e4674080ec2c8ce241b913984870adf9e85a8061
Emiliano <emilianomendozaliscano@gmail.com>
2025-02-27 19:00:31 +0100

    Service added

---
+++ Airport/src/main/java/com/example/test/service/PlaneService.java
@@ -0,0 +1,36 @@
+package com.example.test.service;
+
+import com.example.test.model.Plane;
+import com.example.test.repository.PlaneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PlaneService {
+
+    @Autowired
+    private PlaneRepository planeRepository;
+
+    public List<Plane> getAllPlanes(){
+
+        return planeRepository.findAll();
+    }
+
+    public Optional <Plane> getPLaneById(String Id){
+
+        return planeRepository.findById(Id);
+    }
+
+    public void deleteById(String Id){
+
+        planeRepository.deleteById(Id);
+    }
+
+    public Plane createPlane(Plane plane){
+
+        return planeRepository.save(plane);
+    }
+}
